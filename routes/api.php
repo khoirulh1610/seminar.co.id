b173a79f96b1e45223615ff4529d4f84bdb1963a
khoirul hamdani <khoirulh1610@gmail.com>
2021-10-22 08:18:19 +0700

    bca

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/event','ApiseminarController@index');
+Route::post('/register','ApiseminarController@register');
+
+Route::get('/provinsi','ApiseminarController@provinsi');
+Route::get('/kabupaten','ApiseminarController@kabupaten');
