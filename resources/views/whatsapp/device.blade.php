2a9c6acf74e5d19fb7b8f4e37e3d38211139388f
khoirul hamdani <khoirulh1610@gmail.com>
2021-10-23 20:39:57 +0700

    device

--- resources/views/whatsapp/device.blade.php
+++ resources/views/whatsapp/device.blade.php
@@ -16,9 +16,9 @@
                             @if(Auth::user()->role_id==1)
                             <a href="" data-toggle="modal" data-target="#devicebaru" class="btn btn-xs btn-info btn-rounded m-1">Device Baru</a>                            
                                 @if(\Request()->all)
-                                    <a href="{{url('/device/device?all=Y')}}" class="btn btn-xs btn-info btn-rounded m-1">Tampilkan Semua</a>
-                                @else
                                     <a href="{{url('/device/device')}}" class="btn btn-xs btn-info btn-rounded m-1">Hide Lainnya</a>
+                                @else
+                                    <a href="{{url('/device/device?all=Y')}}" class="btn btn-xs btn-info btn-rounded m-1">Tampilkan Semua</a>
                                 @endif
                             @endif
                         </div>
