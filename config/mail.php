708b09a97dd955228021105e0ea944c49777b9c5
khoirul hamdani <khoirulh1610@gmail.com>
2021-10-26 12:23:10 +0700

    ok

--- config/mail.php
+++ config/mail.php
@@ -43,13 +43,6 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
-            'stream' => [
-                'ssl' => [
-                   'allow_self_signed' => true,
-                   'verify_peer' => false,
-                   'verify_peer_name' => false,
-                ],
-             ],
         ],
 
         'ses' => [
@@ -85,6 +78,13 @@ return [
                 'log',
             ],
         ],
+        'stream' => [
+            'ssl' => [
+               'allow_self_signed' => true,
+               'verify_peer' => false,
+               'verify_peer_name' => false,
+            ],
+         ],
     ],
 
     /*
