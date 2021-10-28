50f0570f235d5a5e3ce7c99d823b21142e730893
khoirul hamdani <khoirulh1610@gmail.com>
2021-10-28 09:08:22 +0700

    dw

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -161,7 +161,7 @@ class DeviceController extends Controller
                     <td>No</td>
                     <td>ID</td>
                     <td>Nama</td>
-                    <td>isAdmin</td>                    
+                    <td>isAdmin</td>                             
                 </tr>";                
                 $i=1;
                 $ggg[] = ["Group"=>($request->nama ?? $g->data->subject ?? '')];
@@ -172,7 +172,7 @@ class DeviceController extends Controller
                                 <td>'.($kontak->vname ?? '').'</td>
                                 <td>'.($kontak->isAdmin ?? '').'</td>                                
                             </tr>';                            
-                    $ggg[] = ["No"=>$i,"Phone"=>preg_replace('/\D/','',$kontak->jid),"Name"=>($kontak->vname ?? ''),"Isadmin"=>($kontak->isAdmin ?? '')];
+                    $ggg[] = ["No"=>$i,"Phone"=>preg_replace('/\D/','',$kontak->jid),"Name"=>($kontak->vname ?? ''),"Isadmin"=>($kontak->isAdmin ?? ''),"Group"=>($request->nama ?? $g->data->subject ?? '')];
                 }
 
                 echo '<a href="'.url($file).'">Download</a>';
